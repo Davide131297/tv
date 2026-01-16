a6c1a0f5278d70bf66ef6d0c36c6669845504989
davide131297 <chiffi.davide@gmx.de>
2026-01-16 19:13:20 +0100

    lokal env

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Ensure environment variables are available
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "http://localhost:8000";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
